7b57039b93b9be6991163e3a09534f621dc7d872
Alejandro Veitia Ramos <Alejandro Veitia>
2021-02-28 19:54:06 -0500

    update

--- src/tu-envio/tu-envio.resolver.ts
+++ src/tu-envio/tu-envio.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PaginateOutDTO } from 'src/config/pagination/dto/paginate-out.dto';
 import { PaginateDTO } from 'src/config/pagination/dto/paginate.dto';
 import { TuEnvioService } from './tu-envio.service';
@@ -12,6 +12,16 @@ export class TuEnvioResolver {
     @Query(type => String, { description: 'Scraping para tu envio' })
     async tuEnvio(@Args('url') url: string): Promise<String> {
         const tuEnvio = await this.tuEnvioService.getTuEnvio(url);
-        return tuEnvio.data;     
+        return tuEnvio.data;
+    }
+
+    @Mutation(type => String, { description: 'Scraping para tu envio' })
+    async tuEnvioLogin(
+        @Args('url') url: string,
+        @Args('user') user: string,
+        @Args('pass') pass: string,
+    ): Promise<String> {
+        const tuEnvio = await this.tuEnvioService.login(url, user, pass);
+        return tuEnvio;
     }
 }
--- src/tu-envio/tu-envio.service.ts
+++ src/tu-envio/tu-envio.service.ts
@@ -10,7 +10,13 @@ export class TuEnvioService {
     }
 
     async getTuEnvio(url: string): Promise<any> {
-        const axios = require('axios')
+        const axios = require('axios');
+        return await axios.get(url);
+
+    }
+
+    async login(url: string, user: string, pass: string): Promise<any> {
+        const axios = require('axios');
         try {
             const response = await axios.get(url);
             return response;
@@ -18,5 +24,32 @@ export class TuEnvioService {
             console.error(error)
         }
         return null;
+    }
+
+    async analisisCharptar(){
+        var Jimp = require("jimp");
+        var file = __dirname + "/imagenGenerada.jpg";
+        Jimp.read(__dirname + '/imagenInicial.jpeg', function (err, image) {
+            if (err) throw err;
+            image.brightness(-.45).contrast(1).write(file, function(err,image){ //Type1
+                if (err) throw err;
+            });
+        });
+    }
+
+    async reconocerTextoImagen() {
+        var Tesseract = require('tesseract.js')
+        var Jimp = require("jimp");
+        var file = __dirname + "/imagenGenerada.jpg";
+        Jimp.read(__dirname + '/imagenInicial.jpeg', function (err, image) {
+            if (err) throw err;
+            image.brightness(-.45).contrast(1).write(file, function(err,image){ //Type1
+                if (err) throw err;
+                Tesseract.recognize(file)
+                  .then(function(result){
+                      console.log(result.text.trim());
+                  })
+            });
+        });
     }
 }
